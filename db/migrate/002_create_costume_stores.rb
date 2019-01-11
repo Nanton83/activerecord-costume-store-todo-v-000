acfc169adde3ed82039da593269c0b985522c0be
Nick Romans <nicholas.romans@gmail.com>
2019-01-11 17:54:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.timestamps
-      t.datetime :closing_time
+      
 
     end
   end
