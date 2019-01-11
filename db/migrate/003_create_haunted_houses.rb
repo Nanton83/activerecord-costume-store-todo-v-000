d3bc9238cf98ba83e996bf0f757529df187dcc56
Nick Romans <nicholas.romans@gmail.com>
2019-01-11 18:29:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.integer :price
       t.boolean :family_friendly
       t.date :opening_date
-      t.date :closign_date
+      t.date :closing_date
     end
   end
 end
