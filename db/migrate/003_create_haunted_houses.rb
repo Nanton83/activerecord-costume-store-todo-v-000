a1b1e807d179056774eb183f599e5717db8c6404
Nick Romans <nicholas.romans@gmail.com>
2019-01-11 18:22:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouse < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_house do |t|
+      t.string :name
+      t.float :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.date :opening_date
+      t.date :closing_date
+      t.text :description
+    end
+  end
+end
